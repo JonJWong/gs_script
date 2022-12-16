d6c6eacdd1b29bb8a87f6b195b8afd4795da728b
JonJWong <wongjonathan01@gmail.com>
2022-12-15 21:37:52 -0500

    Add comments, clean up and organize

--- fs.js
+++ fs.js
@@ -3,14 +3,22 @@ const size = require("image-size");
 const iconvlite = require("iconv-lite");
 const readline = require("readline-sync");
 
-// Change this to your desired pack directory with no / at the end
-// windows: 'C:/Games/Stepmania 5/Stamina RPG 6'
-// macOSX: '/Users/jonmbp/Desktop/shit'
+/* Change this to your desired pack directory with no / at the end
+Windows: 'C:/Games/Stepmania 5/Stamina RPG 6'
+macOSX: '/Users/jonmbp/Desktop/shit' */
 const rootDir = "C:/Users/China/Desktop/b2";
 
-// Banner aspect ratio should be 2.56:1
-// Background aspect ratios can be 4:3, 16:9, 16:10, 5:4
+/* Background aspect ratios can be 4:3, 16:9, 16:10, 5:4 but for now we're not
+  checking those */
 const IMAGE_FILENAME_EXTENSIONS = [".png", ".jpeg", ".jpg", ".mp4"];
+// Banner aspect ratio should be 2.56:1
+const DESIRED_BANNER_ASPECT_RATIO = 2.56;
+// We want 2.36 < aspect ratio < 2.76
+const DESIRED_BANNER_AR_TOLERANCE = 0.2;
+
+/*------------------------------------------------------------------------------
+EXCEPTION "CLASSES"
+------------------------------------------------------------------------------*/
 
 function ParserException(message, fileName) {
   this.fileName = fileName;
@@ -24,12 +32,17 @@ function WriterException(message, fileName) {
   this.name = "WriterException";
 }
 
-// recursive GCD function helper
+/*------------------------------------------------------------------------------
+HELPERS
+------------------------------------------------------------------------------*/
+
+// Recursive GCD function helper
 function greatestCommonDenominator(a, b = 0) {
   if (b === 0) return a;
   return greatestCommonDenominator(b, a % b);
 }
 
+// Finds aspect ratio of an image in a decimal.
 function findAspectRatio(w, h) {
   if (!w || !h) throw "error with file dimensions";
   const gcd = greatestCommonDenominator(w, h);
@@ -38,18 +51,15 @@ function findAspectRatio(w, h) {
   return width / height;
 }
 
+// Function to compare aspect ratio float with desired one for banners (2.56)
 function checkAspectRatio(ratio) {
-  let val = 2.56 - ratio;
+  let val = DESIRED_BANNER_ASPECT_RATIO - ratio;
   if (val < 0) val = -val;
-  return val < 0.2;
+  return val < DESIRED_BANNER_AR_TOLERANCE;
 }
 
-function findAndCheckAr(file) {
-  const { width, height } = size(file);
-  const ar = findAspectRatio(width, height);
-  return checkAspectRatio(ar);
-}
-
+/* Accepts a filename as a string, and checks the ending against the
+  image filename extensions constant */
 function checkIfImage(string) {
   for (let extension of IMAGE_FILENAME_EXTENSIONS) {
     if (string.endsWith(extension)) {
@@ -59,16 +69,26 @@ function checkIfImage(string) {
   return false;
 }
 
-function compareChars(s1, s2) {
-  if (!s1 || !s2) return Infinity;
+/* Accepts a URL, and uses image-size to get the dimensions of an image file.
+  returns false if file is not an image for redundancy. */
+function findAndCheckAr(file) {
+  if (!checkIfImage(file)) return false;
+  const { width, height } = size(file);
+  const ar = findAspectRatio(width, height);
+  return checkAspectRatio(ar);
+}
+
+// compares two strings, returning the number of different characters between them.
+function compareChars(string1, string2) {
+  if (!string1 || !string2) return Infinity;
   const count = {};
 
-  for (let char of s1) {
+  for (let char of string1) {
     count[char] ||= 0;
     count[char]++;
   }
 
-  for (let char of s2) {
+  for (let char of string2) {
     count[char]--;
   }
 
@@ -82,6 +102,38 @@ function compareChars(s1, s2) {
   return sum;
 }
 
+/* I hate this so much.
+  For some reasons certain packs have certain line endings in their .sm/.ssc files
+  might be operating system based and I'm not sure of a better way to check and split
+  the fetched filedata with it, so this is a brute-force approach.
+  Function to take in a data string from a file. File is read with iconvlite,
+  decoded from utf-8, and put into a string. That string is then passed into
+  this function that splits it into an array, so we can iterate the lines
+  and mutate them. */
+function splitFile(data) {
+  let arr = data.split("\n");
+  if (arr[0].endsWith("\r")) {
+    arr = data.split("\r\n");
+  }
+  return arr;
+}
+
+// Function to save and write to file
+function saveChanges(dir, fileData, fileName) {
+  console.log(`Changes were made to ${fileName}, saving...`);
+  const saveData = fileData.join("\n");
+
+  console.log(`Saving ${fileName}`);
+  fs.writeFileSync(dir, saveData);
+}
+
+/*------------------------------------------------------------------------------
+MODULES
+------------------------------------------------------------------------------*/
+
+/* Accepts an array of song folder items, fetched by fs.readdirSync called on
+  rootDir. Returns an object of two arrays which are the songFolders and Files
+  from rootDir. */
 function getSongFoldersFiles(songFolderItems) {
   const songFolders = [];
   const files = [];
@@ -90,8 +142,8 @@ function getSongFoldersFiles(songFolderItems) {
     const path = rootDir + "/" + songFolderItems[i];
     console.log(`Currently on: ${songFolderItems[i]}`);
 
-    // if current file is a directory (a song folder), add to array
-    // if it is not a directory, it is either an image or file within folder
+    /* If current file is a directory (a song folder), add it to the array.
+      If it is not a directory, it is either an image or file within the folder. */
     if (fs.statSync(path).isDirectory()) {
       console.log(`Adding ${songFolderItems[i]} to songFolders`);
       songFolders.push(songFolderItems[i]);
@@ -104,27 +156,34 @@ function getSongFoldersFiles(songFolderItems) {
   return { songFolders, files };
 }
 
+/* Accepts an array of filenames fetched from fs.readdirSync. Sets
+  two variables, one fallbackBanner, one fallbackBg from images found within
+  rootDir. Also returns an array of image names. */
 function getFallbackImages(files) {
   const images = [];
   let fallbackBanner;
   let fallbackBg;
 
   for (let fileName of files) {
-    // if file is an image, check it's aspect ratio and set it to either banner or bg
+    /* If the file is an image, check its aspect ratio and set it to either
+      a banner (if the aspect ratio is correct), or a background */
     if (checkIfImage(fileName)) {
       if (images.push(fileName)) {
         console.log(`Adding ${fileName} to images`);
       }
 
-      // if it's a banner aspect ratio, set banner variable i.e. './shitbanner.png'
+      /* If the file has the aspect ratio of a banner, and fallbackBanner
+        has not been set, set fallbackBanner to the current file */
       if (findAndCheckAr(rootDir + "/" + fileName) && !fallbackBanner) {
         fallbackBanner = fileName;
         console.log(`Set ${fallbackBanner} as fallback banner`);
-        console.log("Reason: Aspect ratio matched 2.56:1 / 64:25");
+        console.log(
+          `Reason: Aspect ratio matched ${DESIRED_BANNER_ASPECT_RATIO}`
+        );
         continue;
       }
 
-      // catch edge cases where aspect ratio might not fit but has name designation
+      // Catch edge cases where files are designated by name
       if (fileName.endsWith("bn") || fileName.endsWith("banner")) {
         fallbackBanner = fileName;
         console.log(`Set ${fallbackBanner} as fallback banner`);
@@ -142,9 +201,16 @@ function getFallbackImages(files) {
   return { images, fallbackBanner, fallbackBg };
 }
 
+/* Takes in a directory (string), and a target file name (string), reads the
+  directory and returns an array of files with names similar to the target.
+  Raises an exception if the directory is empty or incorrect.  */
 function getSimilarFiles(dir, target) {
   const dirFiles = fs.readdirSync(dir);
   const similarFiles = [];
+
+  if (!dirFiles.length) {
+    throw new ParserException(`${dir} is empty/incorrect, aborting.`, target);
+  }
 
   for (let file of dirFiles) {
     if (compareChars(file, target) < 3) {
@@ -152,66 +218,61 @@ function getSimilarFiles(dir, target) {
     }
   }
 
-  if (!dirFiles.length) {
-    throw new ParserException(`${dir} is empty/incorrect, aborting.`, target);
-  }
-
   return similarFiles;
 }
 
+/* Takes in a song folder's name (string), url (string), and it's parts (array).
+  returns two modified strings based on how many levels nested.
+  i.e. '../assets/banner.png' or '../fallback.png' */
 function getInsertSearchUrl(songFolderName, url, urlParts) {
   let insertUrl;
   let searchDir;
 
   if (urlParts.length === 1) {
-    // image is in songfolder
+    // Image is in songfolder
     searchDir = rootDir + "/" + songFolderName + "/";
     insertUrl = "";
   } else if (url.startsWith("../") && urlParts.length === 2) {
-    // image is in root folder (1 level up from songfolder)
+    // Image is in the root folder (1 level up from songfolder)
     searchDir = rootDir;
     insertUrl = "../";
   } else {
-    // image is elsewhere
+    // Image is elsewhere within the root directory
     searchDir = rootDir + "/" + urlParts.slice(1, -1).join("/");
     insertUrl = urlParts.slice(0, -1).join("/") + "/";
-    // theoretically this should look like '../assets/'
+    // Theoretically this should look like '../assets/'
   }
 
   return [insertUrl, searchDir];
 }
 
-// i hate this
-function splitFile(data) {
-  let arr = data.split("\n");
-  if (arr[0].endsWith("\r")) {
-    arr = data.split("\r\n");
-  }
-  return arr;
-}
+//------------------------------------------------------------------------------
+// SCRIPT BEGINS HERE
+//------------------------------------------------------------------------------
 
 try {
   console.log("Script starting...");
-  // get all files/folders within specified directory in an array
+  // Get all files/folders within specified directory in an array
   let songFolderItems = fs.readdirSync(rootDir);
 
   console.log("Iterating through main directory now");
-  // populate songFolders array and files array
+  // Populate songFolders array and files array
   const { songFolders, files } = getSongFoldersFiles(songFolderItems);
 
   console.log("Finished iterating through main directory");
-  // look through the files for images, for fallback banner and bg
-  // while sorting out all the extraneous files
+  /* Look through the files for images, for fallback banner and bg
+  while sorting out all the extraneous files */
   let { images, fallbackBanner, fallbackBg } = getFallbackImages(files);
 
   console.log("Successfully checked for fallback banner and background");
 
-  // if the banner is found, remove it from images array
+  // If the banner is found, remove it from the images array
   if (fallbackBanner) {
     images = images.filter((image) => image !== fallbackBanner);
   }
 
-  // if there is no fallback background yet and images array has something, set as fallback bg
+  /* If there is no fallback background yet and the images array is not empty,
+    set the first element as the fallbackBg */
   if (!fallbackBg && images.length) {
     fallbackBg = images[0];
     console.log(`Set ${fallbackBg} as fallback background`);
@@ -220,16 +281,16 @@ try {
     );
   }
 
-  // if there is still nothing found, just set to empty string
+  // If there is still nothing found, just set both fallbacks to an empty string.
   fallbackBanner ||= "";
   fallbackBg ||= "";
 
   console.log("Finished searching for fallback banner and background");
 
   console.log("Beginning song folder iteration");
-  // iterate through song folders one by one, look at the SM and cross reference
-  // their desired banner URL to see if it exists or not.
-  // if it does not exist, assign fallbacks if those exist.
+  /* Iterate through song folders one by one, look at the .sm and cross reference
+    their desired banner URL to see if it exists or not.
+    If it does not exist, assign fallbacks if those exist. */
   for (let songFolderName of songFolders) {
     const contents = fs.readdirSync(rootDir + "/" + songFolderName);
     for (let fileName of contents) {
@@ -246,12 +307,13 @@ try {
         let chosenBannerName;
         let bgChosen = false;
         let fileChanged = false;
-        // find and navigate first, then check if proper. if proper, continue to check for bg
-        // if not proper, set fallback
+        /* Iterate through the lines of fileData (the .sm/.ssc file) until 
+          either the #BANNER or #BACKGROUND lines. Validate and set banners and
+          backgrounds once found. */
         for (let i in fileData) {
           const currLine = fileData[i];
           if (currLine.startsWith("#BANNER:")) {
-            // if bannerless, check from inner folder first and set banner from inside.
+            // If bannerless, check and prioritize images within songfolder.
             console.log(`Looking within songfolder for banner`);
             const folderFiles = fs.readdirSync(folderDir);
             for (let folderFile of folderFiles) {
@@ -272,6 +334,7 @@ try {
 
             if (bannerChosen) continue;
 
+            // If there is a banner assigned already, follow the URL.
             console.log("Looking at banner assignment");
             const bnLineUrl = currLine.slice(8, -1);
             const bnUrlParts = bnLineUrl.split("/");
@@ -283,25 +346,21 @@ try {
               bnUrlParts
             );
 
+            // Check the location for the supposed file.
             const bnSearchResults = getSimilarFiles(bnSearchDir, smBnName);
-            // if the .sm points to a folder that contains the file it says it does, move on
-            // set flag so fallback is not assigned to this file
-
             if (bnSearchResults.includes(smBnName)) {
               console.log(
                 `Banner for ${fileName} is where specified in .sm/.ssc, checking aspect ratio.`
               );
-              const { width, height } = size(bnSearchDir + "/" + smBnName);
-              const smBannerAr = findAspectRatio(width, height);
-              if (!checkAspectRatio(smBannerAr))
-                console.log(`Aspect ratio incorrect.`);
-              console.log(`Aspect ratio correct, continuing.`);
-              bannerChosen = true;
-              continue;
+              if (findAndCheckAr(bnSearchDir + "/" + smBnName)) {
+                console.log(`Aspect ratio correct, continuing.`);
+                bannerChosen = true;
+                continue;
+              }
+              console.log(`Aspect ratio incorrect.`);
             }
-
-            // now that we've checked through the specified destination and not found anything
-            // we can check for similar files.
+            /* Now that we've checked through the specified destination and
+              haven't found anything, we can check for similar files. */
             if (!bannerChosen) {
               for (let searchResult of bnSearchResults) {
                 if (compareChars(searchResult, smBnName) < 3) {
@@ -319,7 +378,7 @@ try {
               }
             }
 
-            // when all else fails, if a fallback exists, apply it
+            // When all else fails, if a fallback exists, apply it.
             if (!bannerChosen && fallbackBanner) {
               console.log(
                 `No suitable banner found for ${fileName}, using fallback.`
@@ -330,7 +389,7 @@ try {
           }
 
           if (currLine.startsWith("#BACKGROUND:")) {
-            // if background line is empty, find and set background within songfolder first.
+            // If backgroundless, check and prioritize images within songfolder.
             console.log(`Looking within songfolder for background`);
             const folderFiles = fs.readdirSync(folderDir);
             for (let folderFile of folderFiles) {
@@ -350,6 +409,8 @@ try {
 
             if (bgChosen) continue;
 
+            // If there is a background assigned already, follow the URL.
+            console.log("Looking at background assignment");
             const bgLineUrl = currLine.slice(12, -1);
             const bgUrlParts = bgLineUrl.split("/");
             const smBgName = bgUrlParts[bgUrlParts.length - 1];
@@ -360,9 +421,8 @@ try {
               bgUrlParts
             );
 
+            // Check the location for the supposed file.
             const bgSearchResults = getSimilarFiles(bgSearchDir, smBgName);
-            // if the .sm points to a folder that contains the file it says it does, move on
-            // set flag so fallback is not assigned to this file
             if (bgSearchResults.includes(smBgName)) {
               console.log(
                 `Background for ${fileName} is where specified in .sm/.ssc, continuing.`
@@ -371,8 +431,8 @@ try {
               continue;
             }
 
-            // now that we've checked through the specified destination and not found anything
-            // we can check for similar files.
+            /* Now that we've checked through the specified destination and
+              haven't found anything, we can check for similar files. */
             if (!bgChosen) {
               for (let searchResult of bgSearchResults) {
                 if (compareChars(searchResult, smBgName) < 3) {
@@ -390,7 +450,7 @@ try {
               }
             }
 
-            // when all else fails, if a fallback exists, apply it
+            // When all else fails, if a fallback exists, apply it
             if (!bgChosen && fallbackBg) {
               console.log(
                 `No suitable background found for ${fileName}, using fallback.`
@@ -405,13 +465,8 @@ try {
             break;
           }
         }
-
         if (fileChanged) {
-          console.log(`Changes were made to ${fileName}, saving...`);
-          const saveData = fileData.join("\n");
-
-          console.log(`Saving ${fileName}`);
-          fs.writeFileSync(stepFileDir, saveData);
+          saveChanges(stepFileDir, fileData, fileName);
         } else {
           console.log(`No changes were made to ${fileName}`);
         }
@@ -419,7 +474,7 @@ try {
     }
   }
 
-  console.log("Execution finished for now");
+  console.log("Execution finished");
 } catch (error) {
   if (error.name) {
     switch (error.name) {
